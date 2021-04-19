e980d5c52275efabfe6e01dcef00e29f3cf01eb8
Srujal Kachhela <srujalk@vconstruct.in>
2021-04-19 23:01:02 +0530

    Fixed the Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
-import { About } from './pages/About'
+// import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <div>
       <Navbar />
       <div className="container">
-        <Switch>
-          <Route path="/" component={Home} exact />
+        <Home />
+        {/* <Switch>
+          <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-        </Switch>
+        </Switch> */}
       </div>
-    </BrowserRouter>
+    </div>
   )
 }
 
